44d9e03deb18c750e407e73e3c1707475b07c90b
Collin <cdchamberlin.rva@gmail.com>
2023-10-07 16:50:59 -0400

    Added job experiences.

---
+++ scr/Lab 4 + 5/resume-js.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById().style.width = "250px";
+    document.getElementById().style.marginLeft = "250px";
+}
+
+function closeNav(){
+    document.getElementById().style.width = "0";
+    document.getElementById().style.marginLeft = "0";
+}
