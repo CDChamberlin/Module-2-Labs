9ddfced3e957e62e7f95143cd4779f14ac00533e
Collin <cdchamberlin.rva@gmail.com>
2023-10-11 13:32:56 -0400

    Made Sidebar collapsable. Picture is a circle now, but size is not correct.

--- scr/Lab 4 + 5/sript.js
+++ scr/Lab 4 + 5/sript.js
@@ -1,11 +1,11 @@
 function openNav(){
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById().style.marginLeft = "250px";
+    document.getElementById("sidebar").style.cssText="width:250px; display:content;";
+    document.getElementById("main").style.marginLeft = "250px";
     console.log("Clicked Open");
 }
 
 function closeNav(){
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById().style.marginLeft = "0";
+    document.getElementById("sidebar").style.cssText="width:0; display:none;";
+    document.getElementById("main").style.marginLeft = "0";
     console.log("CLicked closed");
 }
