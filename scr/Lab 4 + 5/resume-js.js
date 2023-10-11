6684660eba55f72f3c32679a1baa3e76dfeae253
Collin <cdchamberlin.rva@gmail.com>
2023-10-11 12:54:18 -0400

    Renamed the style and script files. Added the id to the JS file. Created open and close buttons for the side bar.

--- scr/Lab 4 + 5/resume-js.js
+++
@@ -1,9 +0,0 @@
-function openNav(){
-    document.getElementById().style.width = "250px";
-    document.getElementById().style.marginLeft = "250px";
-}
-
-function closeNav(){
-    document.getElementById().style.width = "0";
-    document.getElementById().style.marginLeft = "0";
-}
---
+++ scr/Lab 4 + 5/sript.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById().style.marginLeft = "250px";
+    console.log("Clicked Open");
+}
+
+function closeNav(){
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById().style.marginLeft = "0";
+    console.log("CLicked closed");
+}
