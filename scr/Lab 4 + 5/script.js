0d1bc48e43b4e99a4e4b8679dc713706f4008b6a
Collin <cdchamberlin.rva@gmail.com>
2023-10-15 23:08:12 -0400

    Added the responsive parts and it works well. Issue when opening and closing sideba on small screens.

--- scr/Lab 4 + 5/script.js
+++ scr/Lab 4 + 5/script.js
@@ -1,8 +1,9 @@
 function openNav(){
-    document.getElementById("sidebar").style.cssText="width:250px; display:content; padding:15px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidebar").style.cssText="width:250px; display:content; padding:15px; border: 1px solid #ddd;";
+    document.getElementById("main").style.marginLeft = "282px";
     document.getElementById("openNav").style.display = "none";
 }
+
 
 function closeNav(){
     document.getElementById("sidebar").style.cssText="width:0; display:none;";
@@ -28,3 +29,41 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 } 
+
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+		<div class="carousel__nav">
+			${buttonsHtml.join("")}
+		</div>
+	`
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      // un-select all the items
+      items.forEach((item) =>
+        item.classList.remove("carousel__item--selected")
+      );
+      buttons.forEach((button) =>
+        button.classList.remove("carousel__button--selected")
+      );
+
+      items[i].classList.add("carousel__item--selected");
+      button.classList.add("carousel__button--selected");
+    });
+  });
+
+  // Select the first item on page load
+  items[0].classList.add("carousel__item--selected");
+  buttons[0].classList.add("carousel__button--selected");
+});
+
